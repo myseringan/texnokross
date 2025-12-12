b15ef414dd7c5632aaf22e5708d28de73e620337
Temur Eshmurodov <temureshmurodov2006@gmail.com>
2025-12-13 04:30:21 +0500

    Create ecosystem.config.js

---
+++ server/ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  apps: [{
+    name: 'texnokross-api',
+    script: 'index.js',
+    env: {
+      TELEGRAM_BOT_TOKEN: '8095102367:AAERemO07E-9MrxmAFi1ypuNDLWUgK3rW-Y',
+      TELEGRAM_CHAT_ID: '1273160896',
+      PORT: 3001
+    }
+  }]
+};
