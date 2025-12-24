3f6e94db9b496f36dfde8cd4f01c2e130b824422
Temur Eshmurodov <temureshmurodov2006@gmail.com>
2025-12-25 01:45:19 +0500

    Update ecosystem.config.js

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -13,28 +13,13 @@ module.exports = {
       // Payme Configuration
       // ВАЖНО: Замените на свои данные из личного кабинета Payme Business
       // https://business.payme.uz/
-      PAYME_MERCHANT_ID: '', // ID мерчанта (получите в кабинете)
-      PAYME_SECRET_KEY: '', // Ключ для продакшена
-      PAYME_SECRET_KEY_TEST: '', // Ключ для тестов
+      PAYME_MERCHANT_ID: '694a411d30511f18816ddf45', // ID мерчанта (получите в кабинете)
+      PAYME_SECRET_KEY: '4??45nwPdhSDzN7DCfvd4qMvhD#fuYwT0bIy', // Ключ для продакшена
+      PAYME_SECRET_KEY_TEST: 'yBv?6tS4jCd6oAbBj&Of2xW%1EJVe9mJJWfx', // Ключ для тестов
       PAYME_TEST_MODE: 'true', // Переключить на 'false' для продакшена
       
       // Frontend URL для редиректа после оплаты
-      FRONTEND_URL: 'https://your-domain.uz'
-    },
-    
-    // Production environment
-    env_production: {
-      NODE_ENV: 'production',
-      TELEGRAM_BOT_TOKEN: '8095102367:AAERemO07E-9MrxmAFi1ypuNDLWUgK3rW-Y',
-      TELEGRAM_CHAT_ID: '1273160896',
-      PORT: 3001,
-      
-      PAYME_MERCHANT_ID: '', // Ваш Merchant ID
-      PAYME_SECRET_KEY: '', // Ваш секретный ключ
-      PAYME_SECRET_KEY_TEST: '',
-      PAYME_TEST_MODE: 'false', // В продакшене - false
-      
-      FRONTEND_URL: 'https://your-domain.uz'
+      FRONTEND_URL: 'https://texnokross.uz'
     }
   }]
 };
